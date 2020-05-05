033c12bc85f944aecdf5fec4f842763041318043
Dracks <eclat.nathan@gmail.com>
2020-05-05 13:02:14 +0200

    add grid

--- public/js/main.js
+++ public/js/main.js
@@ -22,3 +22,20 @@ socket.on('chat', (data) => {
 	  		$('#chat').append('<li class="collection-item grey-text text-darken-3">'+data.id.substring(0,12)+" : "+data.msg+'</li>')	
 	  	$('.chatbox').scrollTop($('.chatbox').height());
 	  })
+
+function resize_table_chess( id )
+{
+  $(id).width( 'auto' ).height( 'auto' );
+  $(id+' td, '+id+' th').width( 'auto' ).height( 'auto' ).css({ 'font-size':0.1+'em' });
+  // on redimensionne pour que le plateau tienne dans la fenêtre
+  var sizT = Math.max( Math.max( $(id).width(), $(id).height()), Math.min( $(window).width(), $(window).height())- 60 ); // -40px : marge
+  $(id).width( sizT ).height( sizT );
+  // on redimensionne les cases ET les pièces du jeu
+  var maxWH = sizT/10; // 10x10 cases
+  $(id+' td, '+id+' th').width( maxWH ).height( maxWH );
+  $(id+' td').css({ 'font-size':Math.floor(100*maxWH/16/1.4)/100+'em' });
+  $(id+' th').css({ 'font-size':Math.floor(100*maxWH/16/2.5)/100+'em' });
+};
+$(window).on( 'load resize', function(){
+	resize_table_chess('.chess_table');
+});
